69e0b695e366ff0b9a9c50e7c9057e1ed77e4732
xnuinside <xnuinside@gmail.com>
2020-02-11 20:56:35 +0300

    in process

--- tests/code_sample/done/conditions.py
+++ tests/code_sample/done/conditions.py
@@ -2,7 +2,7 @@
 class CustomException(Exception):
     pass
 
-
+"""
 def one_condition_standard_exception(arg1):
     if arg1 == '1':
         raise Exception('we hate 1')
@@ -30,3 +30,16 @@ def one_condition_custom_exception_and_return_binary_op(arg1, arg2=123, arg3=123
     var = 1
     alias = var
     return arg1 * arg2 + arg3, var * arg1 * alias - 2
+"""
+
+
+def one_condition_custom_exception_and_return_binary_op_and_key(arg1, arg2, arg3):
+    arg3['number'] += 1
+    if arg1 == '1':
+        raise CustomException('we hate 1')
+    elif arg1 > 2:
+        print(f'{arg1} more when 2')
+        return arg1
+    var = 1
+    alias = var
+    return arg1 * arg2 + arg3['number'], var * arg1 * alias - 2
